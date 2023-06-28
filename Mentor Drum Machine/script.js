0283db23c88a196570835cb46b7d569059c4c612
Su9Set <alexx.su9set@gmail.com>
2023-06-28 10:37:17 +0500

    Изменения по свежим правкам + коменты

--- Mentor Drum Machine/script.js
+++ Mentor Drum Machine/script.js
@@ -1,21 +1,19 @@
-const button = ['q', 'w', 'e', 'a', 's', 'd', 'z', 'x', 'c']
+const keys = ['q', 'w', 'e', 'a', 's', 'd', 'z', 'x', 'c']
 
-      function play(pressedKey) {
-        document.querySelector('.last-used-key').innerText = pressedKey.toUpperCase();
-        let formattedPressedKey = pressedKey.toLowerCase();
-
-        var audios = document.querySelectorAll('audio[data-key]');
-        audios.forEach(audio =>{
-            if (pressedKey === audio.dataset.key) {
-                audio.play();
-            }
-        });
-
-      }
-      window.document.onkeyup = function(event) {
-        let pressedKey = event.key.toLowerCase();
-
-        if (button.includes(pressedKey)){
-            play(pressedKey);
+function play(pressedKey) {
+    document.querySelector('.last-used-key').innerText = pressedKey.toUpperCase();
+    let formattedPressedKey = pressedKey.toLowerCase();
+    var audios = document.querySelectorAll('audio[data-key]');
+    audios.forEach(audio =>{
+        if (pressedKey === audio.dataset.key) {
+            audio.play();
         }
-    }
+});
+
+}
+    window.document.onkeyup = function(event) {
+        let pressedKey = event.key.toLowerCase();
+            if (keys.includes(pressedKey)){
+                play(pressedKey);
+            }
+}
